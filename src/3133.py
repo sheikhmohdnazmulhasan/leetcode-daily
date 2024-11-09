b3e4ef00e365a5252387f63eb3944faa1e8961da
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-11-09 12:13:02 +0600

    3133. Minimum Array End

---
+++ src/3133.py
@@ -0,0 +1,17 @@
+# 3133. Minimum Array End
+
+
+class Solution:
+  def minEnd(self, n: int, x: int) -> int:
+   
+    kMaxBit = n.bit_length() + x.bit_length()
+    k = n - 1
+    kBinaryIndex = 0
+
+    for i in range(kMaxBit):
+      if x >> i & 1 == 0:
+        if k >> kBinaryIndex & 1:
+          x |= 1 << i
+        kBinaryIndex += 1
+
+    return x
