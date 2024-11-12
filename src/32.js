66e75953a8ff73f5b5ecbe86dce1a4e56fb01d40
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2024-11-12 15:35:43 +0600

    x

--- src/32.js
+++ src/32.js
@@ -1,2 +1,21 @@
 // 32. Longest Valid Parentheses
 // https://leetcode.com/problems/longest-valid-parentheses/
+
+function longestValidParentheses(s) {
+  let maxLength = 0;
+  const stack = [-1]; // Initialize stack with -1 as base index
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      stack.push(i); // Push the index of '('
+    } else {
+      stack.pop(); // Pop for ')'
+      if (stack.length === 0) {
+        stack.push(i); // Push current index as base for next valid substring
+      } else {
+        maxLength = Math.max(maxLength, i - stack[stack.length - 1]);
+      }
+    }
+  }
+  return maxLength;
+}
